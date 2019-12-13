b92412a2a0f33f67c105fb78d23c2c47483af9b6
Michael Duda <duda@ucar.edu>
2019-12-13 11:05:19 -0700

    Add initial API implementation for the SMIOL C interface.

---
+++ src/smiol.c
@@ -0,0 +1,242 @@
+#include <stddef.h>
+#include "smiol.h"
+
+
+/********************************************************************************
+ *
+ * SMIOL_init
+ *
+ * Initialize a SMIOL context.
+ *
+ * Detailed description.
+ *
+ ********************************************************************************/
+int SMIOL_init(void)
+{
+	return 0;
+}
+
+
+/********************************************************************************
+ *
+ * SMIOL_finalize
+ *
+ * Finalize a SMIOL context.
+ *
+ * Detailed description.
+ *
+ ********************************************************************************/
+int SMIOL_finalize(void)
+{
+	return 0;
+}
+
+
+/********************************************************************************
+ *
+ * SMIOL_inquire
+ *
+ * Inquire about a SMIOL context.
+ *
+ * Detailed description.
+ *
+ ********************************************************************************/
+int SMIOL_inquire(void)
+{
+	return 0;
+}
+
+
+/********************************************************************************
+ *
+ * SMIOL_open_file
+ *
+ * Opens a file within a SMIOL context.
+ *
+ * Detailed description.
+ *
+ ********************************************************************************/
+int SMIOL_open_file(void)
+{
+	return 0;
+}
+
+
+/********************************************************************************
+ *
+ * SMIOL_close_file
+ *
+ * Closes a file within a SMIOL context.
+ *
+ * Detailed description.
+ *
+ ********************************************************************************/
+int SMIOL_close_file(void)
+{
+	return 0;
+}
+
+
+/********************************************************************************
+ *
+ * SMIOL_define_dim
+ *
+ * Defines a new dimension in a file.
+ *
+ * Detailed description.
+ *
+ ********************************************************************************/
+int SMIOL_define_dim(void)
+{
+	return 0;
+}
+
+
+/********************************************************************************
+ *
+ * SMIOL_inquire_dim
+ *
+ * Inquires about an existing dimension in a file.
+ *
+ * Detailed description.
+ *
+ ********************************************************************************/
+int SMIOL_inquire_dim(void)
+{
+	return 0;
+}
+
+
+/********************************************************************************
+ *
+ * SMIOL_define_var
+ *
+ * Defines a new variable in a file.
+ *
+ * Detailed description.
+ *
+ ********************************************************************************/
+int SMIOL_define_var(void)
+{
+	return 0;
+}
+
+
+/********************************************************************************
+ *
+ * SMIOL_inquire_var
+ *
+ * Inquires about an existing variable in a file.
+ *
+ * Detailed description.
+ *
+ ********************************************************************************/
+int SMIOL_inquire_var(void)
+{
+	return 0;
+}
+
+
+/********************************************************************************
+ *
+ * SMIOL_put_var
+ *
+ * Writes a variable to a file.
+ *
+ * Detailed description.
+ *
+ ********************************************************************************/
+int SMIOL_put_var(void)
+{
+	return 0;
+}
+
+
+/********************************************************************************
+ *
+ * SMIOL_get_var
+ *
+ * Reads a variable from a file.
+ *
+ * Detailed description.
+ *
+ ********************************************************************************/
+int SMIOL_get_var(void)
+{
+	return 0;
+}
+
+
+/********************************************************************************
+ *
+ * SMIOL_define_att
+ *
+ * Defines a new attribute in a file.
+ *
+ * Detailed description.
+ *
+ ********************************************************************************/
+int SMIOL_define_att(void)
+{
+	return 0;
+}
+
+
+/********************************************************************************
+ *
+ * SMIOL_inquire_att
+ *
+ * Inquires about an attribute in a file.
+ *
+ * Detailed description.
+ *
+ ********************************************************************************/
+int SMIOL_inquire_att(void)
+{
+	return 0;
+}
+
+
+/********************************************************************************
+ *
+ * SMIOL_file_sync
+ *
+ * Forces all in-memory data to be flushed to disk.
+ *
+ * Detailed description.
+ *
+ ********************************************************************************/
+int SMIOL_file_sync(void)
+{
+	return 0;
+}
+
+
+/********************************************************************************
+ *
+ * SMIOL_error_string
+ *
+ * Returns an error string for a specified error code.
+ *
+ * Detailed description.
+ *
+ ********************************************************************************/
+const char *SMIOL_error_string(void)
+{
+	return NULL;
+}
+
+
+/********************************************************************************
+ *
+ * SMIOL_set_option
+ *
+ * Sets an option for the SMIOL library.
+ *
+ * Detailed description.
+ *
+ ********************************************************************************/
+int SMIOL_set_option(void)
+{
+	return 0;
+}
---
+++ src/smiol.h
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * SMIOL -- The Simple MPAS I/O Library
+ *******************************************************************************/
+
+/*
+ * Types
+ */
+struct {
+} SMIOL_context;
+
+struct {
+} SMIOL_file;
+
+/*
+ * Library methods
+ */
+int SMIOL_init(void);
+int SMIOL_finalize(void);
+int SMIOL_inquire(void);
+
+/*
+ * File methods
+ */
+int SMIOL_open_file(void);
+int SMIOL_close_file(void);
+
+/*
+ * Dimension methods
+ */
+int SMIOL_define_dim(void);
+int SMIOL_inquire_dim(void);
+
+/*
+ * Variable methods
+ */
+int SMIOL_define_var(void);
+int SMIOL_inquire_var(void);
+int SMIOL_put_var(void);
+int SMIOL_get_var(void);
+
+/*
+ * Attribute methods
+ */
+int SMIOL_define_att(void);
+int SMIOL_inquire_att(void);
+
+/*
+ * Control methods
+ */
+int SMIOL_file_sync(void);
+const char *SMIOL_error_string(void);
+int SMIOL_set_option(void);
