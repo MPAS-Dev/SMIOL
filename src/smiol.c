MPAS-Dev/SMIOL

commit debaab0d3962578d9ae32005581db93e5401c7d3

Define three new error codes: SMIOL_{INVALID_ARGUMENT, MPI_ERROR, FORTRAN_ERROR}

src/smiol.c
@@ -310,6 +310,12 @@ const char *SMIOL_error_string(int errno)
 		return "Success!";
 	case SMIOL_MALLOC_FAILURE:
 		return "malloc returned a null pointer";
+	case SMIOL_INVALID_ARGUMENT:
+		return "invalid subroutine argument";
+	case SMIOL_MPI_ERROR:
+		return "internal MPI call failed";
+	case SMIOL_FORTRAN_ERROR:
+		return "Fortran wrapper detected an inconsistency in C return values";
 	default:
 		return "Unknown error";
 	}

smiol_runner.F90
@@ -100,11 +100,17 @@ program smiol_runner
         stop 1
     endif
 
-    write(0,*) "Testing SMIOLf_error_string success: ", trim(SMIOLf_error_string(SMIOL_SUCCESS))
-    write(0,*) "Testing SMIOLf_error_string unkown error: ", trim(SMIOLf_error_string(1))
-    write(0,*) "Testing SMIOLf_error_string malloc returned a null pointer: ", &
+    write(0,'(a)') "Testing SMIOLf_error_string success: ", trim(SMIOLf_error_string(SMIOL_SUCCESS))
+    write(0,'(a)') "Testing SMIOLf_error_string unkown error: ", trim(SMIOLf_error_string(1))
+    write(0,'(a)') "Testing SMIOLf_error_string malloc returned a null pointer: ", &
                trim(SMIOLf_error_string(SMIOL_MALLOC_FAILURE))
-    write(0,*) "SUCCESS"
+    write(0,'(a)') "Testing SMIOL_error_string test invalid subroutine argument: ", &
+               trim(SMIOLf_error_string(SMIOL_INVALID_ARGUMENT))
+    write(0,'(a)') "Testing SMIOL_error_string test internal MPI call failed: ", &
+               trim(SMIOLf_error_string(SMIOL_MPI_ERROR))
+    write(0,'(a)') "Testing SMIOL_error_string test Fortran wrapper detected an inconsistency in C return values: ", &
+               trim(SMIOLf_error_string(SMIOL_FORTRAN_ERROR))
+    write(0,'(a)') "SUCCESS"
 
     if (SMIOLf_finalize(context) /= SMIOL_SUCCESS) then
         write(0,*) "ERROR: 'SMIOLf_finalize' was not called successfully"

smiol_runner.c
@@ -138,6 +138,12 @@ int main(int argc, char **argv)
 	printf("SMIOL_error_string test 'Success!': %s\n", SMIOL_error_string(SMIOL_SUCCESS));
 	printf("SMIOL_error_string test 'malloc returned a null pointer': %s\n",
 		SMIOL_error_string(SMIOL_MALLOC_FAILURE));
+	printf("SMIOL_error_string test 'invalid subroutine argument': %s\n",
+		SMIOL_error_string(SMIOL_INVALID_ARGUMENT));
+	printf("SMIOL_error_string test 'internal MPI call failed': %s\n",
+		SMIOL_error_string(SMIOL_MPI_ERROR));
+	printf("SMIOL_error_string test 'Fortran wrapper detected an inconsistency in C return values': %s\n",
+		SMIOL_error_string(SMIOL_FORTRAN_ERROR));
 
 	if ((ierr = SMIOL_finalize(&context)) != SMIOL_SUCCESS) {
 		printf("ERROR: SMIOL_finalize: %s ", SMIOL_error_string(ierr));
@@ -148,6 +154,7 @@ int main(int argc, char **argv)
 		fprintf(stderr, "SMIOL_finalize returned a non-NULL context\n");
 		return 1;
 	}
+
 	printf("Called all SMIOL functions successfully!\n");
 
 	if (MPI_Finalize() != MPI_SUCCESS) {

src/smiol_codes.inc
@@ -1,2 +1,5 @@
-#define SMIOL_SUCCESS (0)
-#define SMIOL_MALLOC_FAILURE (-1)
+#define SMIOL_SUCCESS           (0)
+#define SMIOL_MALLOC_FAILURE   (-1)
+#define SMIOL_INVALID_ARGUMENT (-2)
+#define SMIOL_MPI_ERROR        (-3)
+#define SMIOL_FORTRAN_ERROR    (-4)

commit bd6b12ad20410b01f1a37605c604af85a23295a4

Make use of new error codes introduced in debaab0d
With the introduction of the error codes SMIOL_INVALID_ARGUMENT,
SMIOL_MPI_ERROR, and SMIOL_FORTRAN_ERROR in debaab0d, undefined error
codes -999, -998, and -997 can be replaced in SMIOL code.

src/smiol.c
@@ -51,7 +51,7 @@ int SMIOL_init(MPI_Comm comm, struct SMIOL_context **context)
 	 * the context pointer is not NULL
 	 */
 	if (context == NULL) {
-		return -999;    /* Should we define an error code for this? */
+		return SMIOL_INVALID_ARGUMENT;
 	}
 
 	/*
@@ -65,7 +65,7 @@ int SMIOL_init(MPI_Comm comm, struct SMIOL_context **context)
 		 */
 		(*context) = NULL;
 
-		return -999;    /* Should we define an error code for this? */
+		return SMIOL_INVALID_ARGUMENT;
 	}
 
 	*context = (struct SMIOL_context *)malloc(sizeof(struct SMIOL_context));
@@ -79,20 +79,20 @@ int SMIOL_init(MPI_Comm comm, struct SMIOL_context **context)
 	if (MPI_Comm_dup(comm, &smiol_comm) != MPI_SUCCESS) {
 		free((*context));
 		(*context) = NULL;
-		return -998;    /* Should we define an error code for this? */
+		return SMIOL_MPI_ERROR;
 	}
 	(*context)->fcomm = MPI_Comm_c2f(smiol_comm);
 
 	if (MPI_Comm_size(smiol_comm, &((*context)->comm_size)) != MPI_SUCCESS) {
 		free((*context));
 		(*context) = NULL;
-		return -998;    /* Should we define an error code for this? */
+		return SMIOL_MPI_ERROR;
 	}
 
 	if (MPI_Comm_rank(smiol_comm, &((*context)->comm_rank)) != MPI_SUCCESS) {
 		free((*context));
 		(*context) = NULL;
-		return -998;    /* Should we define an error code for this? */
+		return SMIOL_MPI_ERROR;
 	}
 
 	return SMIOL_SUCCESS;
@@ -130,7 +130,7 @@ int SMIOL_finalize(struct SMIOL_context **context)
 	if (MPI_Comm_free(&smiol_comm) != MPI_SUCCESS) {
 		free((*context));
 		(*context) = NULL;
-		return -998;
+		return SMIOL_MPI_ERROR;
 	}
 
 	free((*context));
@@ -176,14 +176,14 @@ int SMIOL_open_file(struct SMIOL_context *context, const char *filename, struct 
 	 * the file pointer is not NULL
 	 */
 	if (file == NULL) {
-		return -999;    /* Should we define an error code for this? */
+		return SMIOL_INVALID_ARGUMENT;
 	}
 
 	/*
 	 * Check that context is valid
 	 */
 	if (context == NULL) {
-		return -999;    /* Should we define an error code for this? */
+		return SMIOL_INVALID_ARGUMENT;
 	}
 
 	*file = (struct SMIOL_file *)malloc(sizeof(struct SMIOL_file));
@@ -201,7 +201,7 @@ int SMIOL_open_file(struct SMIOL_context *context, const char *filename, struct 
 				MPI_INFO_NULL, &((*file)->ncidp)) != NC_NOERR) {
 		free((*file));
 		(*file) = NULL;
-		return -996;
+		return -996;    /* TODO: we still need a way to handle library-specific errors */
 	}
 #endif
 
@@ -235,7 +235,7 @@ int SMIOL_close_file(struct SMIOL_file **file)
 	if (ncmpi_close((*file)->ncidp) != NC_NOERR) {
 		free((*file));
 		(*file) = NULL;
-		return -996;
+		return -996;    /* TODO: we still need a way to handle library-specific errors */
 	}
 #endif
 

src/smiolf.F90
@@ -103,7 +103,7 @@ contains
         if (ierr == SMIOL_SUCCESS) then
             if (.not. c_associated(c_context)) then
                 nullify(context)
-                ierr = -997     ! TODO: define an error code for this. The c_context should not be null...
+                ierr = SMIOL_FORTRAN_ERROR
             else
                 call c_f_pointer(c_context, context)
             end if
@@ -111,7 +111,7 @@ contains
             if (.not. c_associated(c_context)) then
                 nullify(context)
             else
-                ierr = -997     ! TODO: define an error code for this. The c_context should be null...
+                ierr = SMIOL_FORTRAN_ERROR
             end if
         end if
 
@@ -157,7 +157,7 @@ contains
 
         if (ierr == SMIOL_SUCCESS) then
             if (c_associated(c_context)) then
-                ierr = -997     ! TODO: define an error code for this. The c_context should be null...
+                ierr = SMIOL_FORTRAN_ERROR
             else
                 nullify(context)
             end if
@@ -251,7 +251,7 @@ contains
         if (ierr == SMIOL_SUCCESS) then
             if (.not. c_associated(c_file)) then
                 nullify(file)
-                ierr = -997     ! TODO: define an error code for this. The c_file should not be null...
+                ierr = SMIOL_FORTRAN_ERROR
             else
                 call c_f_pointer(c_file, file)
             end if
@@ -259,7 +259,7 @@ contains
             if (.not. c_associated(c_file)) then
                 nullify(file)
             else
-                ierr = -997     ! TODO: define an error code for this. The c_file should be null...
+                ierr = SMIOL_FORTRAN_ERROR
             end if
         end if
 
@@ -304,7 +304,7 @@ contains
 
         if (ierr == SMIOL_SUCCESS) then
             if (c_associated(c_file)) then
-                ierr = -997     ! TODO: define an error code for this. The c_file should be null...
+                ierr = SMIOL_FORTRAN_ERROR
             else
                 nullify(file)
             end if