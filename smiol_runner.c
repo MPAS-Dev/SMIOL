0d8f74a430b6201219ef397ea2021120c18af3f3
Miles A Curry <currymiles@gmail.com>
2020-01-07 18:38:50 +0000

    Update smiol_runner.c printf to fprintf

--- smiol_runner.c
+++ smiol_runner.c
@@ -55,7 +55,7 @@ int main(int argc, char **argv)
 
 
 	if ((ierr = SMIOL_init(MPI_COMM_WORLD, &context)) != SMIOL_SUCCESS) {
-		printf("ERROR: SMIOL_init: %s ", SMIOL_error_string(ierr));
+		fprintf(stderr, "ERROR: SMIOL_init: %s ", SMIOL_error_string(ierr));
 		return 1;
 	} 
 
@@ -80,88 +80,103 @@ int main(int argc, char **argv)
 	free(io_elements);
 
 	if ((ierr = SMIOL_free_decomp(&decomp)) != SMIOL_SUCCESS) {
-		printf("ERROR: SMIOL_free_decomp: %s ", SMIOL_error_string(ierr));
+		fprintf(stderr, "ERROR: SMIOL_free_decomp: %s ",
+			SMIOL_error_string(ierr));
 		return 1;
 	}
 
 	if (decomp != NULL) {
-		printf("ERROR: SMIOL_free_decomp - Decomp not 'NULL' after free\n");
+		fprintf(stderr, "ERROR: SMIOL_free_decomp - Decomp not 'NULL' after free\n");
 		return 1;
 	}
 
 	if ((ierr = SMIOL_inquire()) != SMIOL_SUCCESS) {
-		printf("ERROR: SMIOL_inquire: %s ", SMIOL_error_string(ierr));
+		fprintf(stderr, "ERROR: SMIOL_inquire: %s ",
+			SMIOL_error_string(ierr));
 		return 1;
 	}
 
 	if ((ierr = SMIOL_open_file(context, "blah.nc", &file)) != SMIOL_SUCCESS) {
-		printf("ERROR: SMIOL_open_file: %s ", SMIOL_error_string(ierr));
+		fprintf(stderr, "ERROR: SMIOL_open_file: %s ",
+			SMIOL_error_string(ierr));
 		return 1;
 	}
 
 	if ((ierr = SMIOL_close_file(&file)) != SMIOL_SUCCESS) {
-		printf("ERROR: SMIOL_close_file: %s ", SMIOL_error_string(ierr));
+		fprintf(stderr, "ERROR: SMIOL_close_file: %s ",
+			SMIOL_error_string(ierr));
 		return 1;
 	}
 
 	if ((ierr = SMIOL_define_dim()) != SMIOL_SUCCESS) {
-		printf("ERROR: SMIOL_define_dim: %s ", SMIOL_error_string(ierr));
+		fprintf(stderr, "ERROR: SMIOL_define_dim: %s ",
+			SMIOL_error_string(ierr));
 		return 1;
 	}
 
 	if ((ierr = SMIOL_inquire_dim()) != SMIOL_SUCCESS) {
-		printf("ERROR: SMIOL_inquire_dim: %s ", SMIOL_error_string(ierr));
+		fprintf(stderr, "ERROR: SMIOL_inquire_dim: %s ",
+			SMIOL_error_string(ierr));
 		return 1;
 	}
 
 	if ((ierr = SMIOL_define_var()) != SMIOL_SUCCESS) {
-		printf("ERROR: SMIOL_define_var: %s ", SMIOL_error_string(ierr));
+		fprintf(stderr, "ERROR: SMIOL_define_var: %s ",
+			SMIOL_error_string(ierr));
 		return 1;
 	}
 
 	if ((ierr = SMIOL_put_var()) != SMIOL_SUCCESS) {
-		printf("ERROR: SMIOL_put_var: %s ", SMIOL_error_string(ierr));
+		fprintf(stderr, "ERROR: SMIOL_put_var: %s ",
+			SMIOL_error_string(ierr));
 		return 1;
 	}
 
 	if ((ierr = SMIOL_get_var()) != SMIOL_SUCCESS) {
-		printf("ERROR: SMIOL_get_var: %s ", SMIOL_error_string(ierr));
+		fprintf(stderr, "ERROR: SMIOL_get_var: %s ",
+			SMIOL_error_string(ierr));
 		return 1;
 	}
 		
 	if ((ierr = SMIOL_define_att()) != SMIOL_SUCCESS) {
-		printf("ERROR: SMIOL_define_att: %s ", SMIOL_error_string(ierr));
+		fprintf(stderr, "ERROR: SMIOL_define_att: %s ",
+			SMIOL_error_string(ierr));
 		return 1;
 	}
 
 	if ((ierr = SMIOL_inquire_att()) != SMIOL_SUCCESS) {
-		printf("ERROR: SMIOL_inquire_att: %s ", SMIOL_error_string(ierr));
+		fprintf(stderr, "ERROR: SMIOL_inquire_att: %s ",
+			SMIOL_error_string(ierr));
 		return 1;
 	}
 	
 	if ((ierr = SMIOL_file_sync()) != SMIOL_SUCCESS) {
-		printf("ERROR: SMIOL_file_sync: %s ", SMIOL_error_string(ierr));
+		fprintf(stderr, "ERROR: SMIOL_file_sync: %s ",
+			SMIOL_error_string(ierr));
 		return 1;
 	}
 
 	if ((ierr = SMIOL_set_option()) != SMIOL_SUCCESS) {
-		printf("ERROR: SMIOL_set_option: %s ", SMIOL_error_string(ierr));
+		fprintf(stderr, "ERROR: SMIOL_set_option: %s ",
+			SMIOL_error_string(ierr));
 		return 1;
 	}
 
-	printf("SMIOL_error_string test 'Unknown error': %s\n", SMIOL_error_string(-1));
-	printf("SMIOL_error_string test 'Success!': %s\n", SMIOL_error_string(SMIOL_SUCCESS));
-	printf("SMIOL_error_string test 'malloc returned a null pointer': %s\n",
+	fprintf(stderr, "SMIOL_error_string test 'Unknown error': %s\n",
+						SMIOL_error_string(-1));
+	fprintf(stderr, "SMIOL_error_string test 'Success!': %s\n",
+						SMIOL_error_string(SMIOL_SUCCESS));
+	fprintf(stderr, "SMIOL_error_string test 'malloc returned a null pointer': %s\n",
 		SMIOL_error_string(SMIOL_MALLOC_FAILURE));
-	printf("SMIOL_error_string test 'invalid subroutine argument': %s\n",
+	fprintf(stderr, "SMIOL_error_string test 'invalid subroutine argument': %s\n",
 		SMIOL_error_string(SMIOL_INVALID_ARGUMENT));
-	printf("SMIOL_error_string test 'internal MPI call failed': %s\n",
+	fprintf(stderr, "SMIOL_error_string test 'internal MPI call failed': %s\n",
 		SMIOL_error_string(SMIOL_MPI_ERROR));
-	printf("SMIOL_error_string test 'Fortran wrapper detected an inconsistency in C return values': %s\n",
+	fprintf(stderr, "SMIOL_error_string test 'Fortran wrapper detected an inconsistency in C return values': %s\n",
 		SMIOL_error_string(SMIOL_FORTRAN_ERROR));
 
 	if ((ierr = SMIOL_finalize(&context)) != SMIOL_SUCCESS) {
-		printf("ERROR: SMIOL_finalize: %s ", SMIOL_error_string(ierr));
+		fprintf(stderr, "ERROR: SMIOL_finalize: %s ", SMIOL_error_string(ierr));
 		return 1;
 	}
 
@@ -170,7 +185,7 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	printf("Called all SMIOL functions successfully!\n");
+	fprintf(stderr, "Called all SMIOL functions successfully!\n");
 
 	if (MPI_Finalize() != MPI_SUCCESS) {
 		fprintf(stderr, "Error: MPI_Finalize failed.\n");
