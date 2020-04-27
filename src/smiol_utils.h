2cec83ffb312b3af0cdfe6b9ffde7ccd3babc83f
Michael Duda <duda@ucar.edu>
2020-04-27 16:05:44 -0600

    Add utility function get_io_elements
    The get_io_elements function may be used internally by SMIOL to compute
    a contiguous range of I/O elements to be read/written by an MPI task.

--- src/smiol_utils.h
+++ src/smiol_utils.h
@@ -25,6 +25,12 @@ int transfer_field(const struct SMIOL_decomp *decomp, int dir,
                    size_t element_size, const void *in_field, void *out_field);
 
 /*
+ * Field decomposition
+ */
+int get_io_elements(int comm_rank, int num_io_tasks, int io_stride,
+                    size_t n_io_elements, size_t *io_start, size_t *io_count);
+
+/*
  * Debugging
  */
 void print_lists(int comm_rank, SMIOL_Offset *comp_list, SMIOL_Offset *io_list);
